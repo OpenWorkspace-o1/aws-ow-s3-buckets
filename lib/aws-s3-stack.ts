de8d1dd9e007c935dc3b3283a169431b9050a70f
Trung Dinh <xamarindevelopervietnam@outlook.com>
2024-12-18 20:34:38 +0700

    Update stack AwsS3Stack

--- lib/aws-s3-stack.ts
+++ lib/aws-s3-stack.ts
@@ -1,9 +1,56 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { AwsS3StackProps } from './AwsS3StackProps';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class AwsS3Stack extends cdk.Stack {
+  /**
+   * Constructs a new instance of the AwsS3Stack.
+   *
+   * @param scope The parent construct.
+   * @param id An identifier for the stack.
+   * @param props Configuration properties for the stack, including resource prefixes, bucket names, and deployment environment.
+   */
   constructor(scope: Construct, id: string, props: AwsS3StackProps) {
     super(scope, id, props);
+
+    const removalPolicy = props.deployEnvironment === 'production' ? cdk.RemovalPolicy.RETAIN : cdk.RemovalPolicy.DESTROY;
+
+    // define an S3 bucket
+    const s3Bucket = new s3.Bucket(this, `${props.resourcePrefix}-${props.s3BucketName}`, {
+      bucketName: `${props.deployEnvironment}-${props.deployRegion}-${props.s3BucketName}`,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: false,
+      removalPolicy: removalPolicy,
+      autoDeleteObjects: removalPolicy === cdk.RemovalPolicy.DESTROY,
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+      versioned: true, // Enable versioning
+      serverAccessLogsBucket: new s3.Bucket(this, `${props.resourcePrefix}-s3-bucket-logs`, {
+        bucketName: `${props.deployEnvironment}-${props.deployRegion}-s3-bucket-logs`,
+        encryption: s3.BucketEncryption.S3_MANAGED,
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        publicReadAccess: false,
+        removalPolicy: removalPolicy,
+        versioned: false,
+        enforceSSL: true,  // Ensure all requests to the S3 bucket use SSL
+      }),
+      enforceSSL: true,  // Ensure all requests to the S3 bucket use SSL
+      serverAccessLogsPrefix: `${props.resourcePrefix}-${props.deployRegion}-${props.s3BucketName}-logs/`
+    });
+
+    // export s3Bucket name
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-${props.s3BucketName}-Export`, {
+      value: s3Bucket.bucketName,
+      exportName: `${props.deployEnvironment}-${props.deployRegion}-${props.s3BucketName}-Export`,
+      description: 'The name of the S3 bucket.',
+    });
+
+    // export s3Bucket ARN
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-${props.s3BucketName}-Arn-Export`, {
+      value: s3Bucket.bucketArn,
+      exportName: `${props.deployEnvironment}-${props.deployRegion}-${props.s3BucketName}-Arn-Export`,
+      description: 'The ARN of the S3 bucket.',
+    });
   }
 }
